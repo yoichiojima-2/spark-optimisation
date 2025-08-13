6af87d4340209ffd6a8da3a3569e35dc1112a865
yoichi ojima <yoichiojima@gmail.com>
2025-08-13 23:54:31 +0900

    wip: get spark session

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
+import tomllib
+from pyspark.sql import SparkSession
+
+
+class Config:
+    def __init__(self):
+        self.config = self.read_config()
+
+    @property
+    def name(self):
+        return self.config["name"]
+
+    @property
+    def version(self):
+        return self.config["version"]
+
+    def read_config(self):
+        with open("pyproject.toml", "r") as f:
+            pyproject = tomllib.load(f)
+        return pyproject["project"]["version"]
+
+
+def get_spark_session():
+    config = Config()
+    return SparkSession.builder.appName(config.name).getOrCreate()
+
+
 def main():
+    spark = get_spark_session()
+    print(spark)
     print("Hello from spark-optimisation!")
 
 
