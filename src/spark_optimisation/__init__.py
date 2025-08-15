805fe3166dd2e83cb80307729d24a679e0338377
yoichi ojima <yoichiojima@gmail.com>
2025-08-15 15:53:44 +0900

    Update spark optimisation module

--- src/spark_optimisation/__init__.py
+++ src/spark_optimisation/__init__.py
@@ -1,4 +1,4 @@
-from spark_optimisation.lib import Config, get_spark_session, mock_df
+from spark_optimisation.lib import Config, get_spark_session
 from spark_optimisation.main import main
 
-__all__ = ["Config", "get_spark_session", "mock_df", "main"]
+__all__ = ["Config", "get_spark_session", "main"]
--- src/spark_optimisation/main.py
+++ src/spark_optimisation/main.py
@@ -1,15 +1,9 @@
-from pyspark.sql import DataFrame
-
 from spark_optimisation.lib import get_spark_session
 
 
-def read_parquet() -> DataFrame:
-    path = "./artifacts/mock-1"
-    return get_spark_session().read.parquet(path)
-
-
 def main():
-    df = read_parquet()
+    path = "./artifacts/mock-1"
+    df = get_spark_session().read.parquet(path)
     df.show()
 
 
